a86f2458a12fd3cf333b7e30a35c94f358461fc2
Brian Silvestri <brian.silvestri@athenium.com>
2022-09-19 22:41:50 -0500

    cleaned .py

--- wedding_script.py
+++ wedding_script.py
@@ -3,21 +3,31 @@ from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 
 
-def read_in_sheets(sheet_num):
+def get_sheet(sheet_name: str, creds: str):
+    # pass creds and return sheet object
 
-    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name("service_account.json", scope)
-
+    scope = (
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive",
+    )
+    creds = ServiceAccountCredentials.from_json_keyfile_name(creds, scope)
     client = gspread.authorize(creds)
-    sheet = client.open('Master Wedding Planner')
+    sheet = client.open(sheet_name)
+
+    return sheet
+
+
+def read_in_sheets(sheet_num: int) -> pd.DataFrame:
+    # read in sheets to a pandas df
+
     sheet_instance1 = sheet.get_worksheet(sheet_num)
-    data = sheet_instance1.get_all_records()
-    df = pd.DataFrame(data)
+    df = pd.DataFrame(sheet_instance1.get_all_records())
 
     return df
 
 
 if __name__ == "__main__":
 
-    sheet = read_in_sheets(0)
-    print(sheet.head())
+    sheet = get_sheet("Master Wedding Planner", "service_account.json")
+    df = read_in_sheets(0)
+    print(df.head())
