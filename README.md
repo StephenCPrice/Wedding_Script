3626535f5d960d7d9f1b57ecfba1cce2c5579335
stephen <stephencharlesprice@gmail.com>
2022-09-23 10:27:33 -0500

    Will be working on email and fb msg fnctns's next

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Utilizes gspread to pull data from a google sheet and pushes out wedding save the date invitations to those listed on your google sheet.
 ## conda environmet called stephenp python==3.10.4 see requirements.txt for library versions
 ## conda create -n stephenp -r requirements.txt
+
+# Work in progress
+
+# Needs to iterate through the spreadsheet, check that there is a phone number/email/fbmessage attached to each name, and give the user a log of all people that will receive a text/email/fbmessage.
+# Asks users for input regarding the message that they would like to send mentioning they can include url's. 
+# Asks the user if they would like to proceed.
+# Proceeds, and returns a log of all users with whom errors occured. 
+# stores a log of all names that didn't receive a text 'theoretically', although you could theoretically provide validation as well with twilio. 
+
+
+
+
+
+## Future feature: Because it takes Twilio time to que messages and send them, perhaps it would be good to include an interface that would provide validation when sucessfully sent. 
--- wedding_script.py
+++ wedding_script.py
@@ -1,9 +1,13 @@
-#To make it more reusable in the future it seems like using regex to find the headers and setting the head in line 25 to the row location(using gspread) might be good. That can be saved for later though.
+#To make it more reusable in the future it seems like using regex to find the headers and setting the head in line 29 to the row location(using gspread) might be good. That can be saved for later though.
 #Come to think of it, you could use regex to find the headers, and then use that to also locate the exact header names to be used as the keys to iterate over. Let's build a v1 first though.
-import gspread, twilio
+#Will need try and except blocks to return a log of whichever texts fail to send, although there shouldn't be any.
+import gspread, re, os
+from dotenv import load_dotenv  #loads enviornment variables from a given file, needed to share the .env file. Will be used with twilio
 from oauth2client.service_account import ServiceAccountCredentials
+from twilio.rest import Client
 import pandas as pd
 
+load_dotenv('twilio_creds.env')
 
 def get_sheet(sheet_name: str, credentials: str):
     # pass creds and return sheet object
@@ -27,13 +31,26 @@ def read_in_sheets(sheet_num: int) -> pd.DataFrame:
 
     return df
 
+def send_text(text: str, guest_number: str):                                  #Placeholder function: works in it's current state. Numbers may need to be in '+1xxxxxxxxxx' format(not 100%). Must be reformatted to accept a url in the message.
+    client = Client(os.environ['account_sid'], os.environ['auth_token'])      #Using dotenv we are able to use enviornment variables with a scope local to the project.
+    message = client.messages.create(
+            body = text,
+            from_ ='+14783775883',
+            to = guest_number
+         )
+    print(message.sid)
+
 
 if __name__ == "__main__":
 
     sheet = get_sheet("Master Wedding Planner", "service_account.json")
     df = read_in_sheets(0)
     print(df.head())
+    
 
 sheet_composite = read_in_sheets(1)                                  #  Grabs the data one time so as not to exceed google API's query limit.
+
+#Core for loop
 for i in range(len(list(sheet_composite['First Name (s)']))):
-    print((list(sheet_composite['First Name (s)'])[i]))
+    print((list(sheet_composite['First Name (s)'])[i]), (list(sheet_composite['Phone Number'])[i]))
+
